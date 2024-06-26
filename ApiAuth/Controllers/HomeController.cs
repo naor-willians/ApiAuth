1722cbfff5bae227d2851bf7cb0965e8a7647e06
Naor Willians <naorwillians@hotmail.com>
2024-06-26 14:56:58 -0300

    Inicializando

---
+++ ApiAuth/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ApiAuth.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Policy;
+
+namespace ApiAuth.Controllers;
+
+[ApiController]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    [Route("anonymus")]
+    [AllowAnonymous]
+    public string Anonymus() => "Anônimo";
+
+    [HttpGet]
+    [Route("authenticated")]
+    [Authorize]
+    public string Authorize() => $"Autenticado -  {User.Identity.Name}";
+
+    [HttpGet]
+    [Route("employee")]
+    [Authorize(Roles = "employee, manager")]
+    public string Employee() => "Funcionário";
+
+    [HttpGet]
+    [Route("manager")]
+    [Authorize(Roles = "manager")]
+    public string Manager() => "Gerente";
+}
---
+++ ApiAuth/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using ApiAuth.Models;
+using ApiAuth.Repositories;
+using ApiAuth.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAuth.Controllers
+{
+    [ApiController]
+    [Route("v1")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> AuthenticateAsync([FromBody] User model)
+        {
+            //Recupera o user
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            //verifica se o user existe
+            if(user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            var token = TokenService.GenerateToken(user);
+
+            //oculta senha
+            user.Password = "";
+
+            //retorna os dados
+            return new
+            {
+                user = user,
+                token = token,
+            };
+
+
+        }
+    }
+}
